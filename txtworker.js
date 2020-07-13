295da087d3a994682911b42b769caa8b49beb193
etztrefis <etztrefishello@gmail.com>
2020-07-13 11:53:06 +0700

    ru pastas created and wrapped

--- txtworker.js
+++ txtworker.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let query = "";
-fs.readFile("pastas.txt", "utf8", function (error, data) {
+fs.readFile("raw_pastas.txt", "utf8", function (error, data) {
   let separator = data.split("\r\n");
   var sortedSeparator = separator.filter(function (elem, pos) {
     return separator.indexOf(elem) == pos;
@@ -8,7 +8,7 @@ fs.readFile("pastas.txt", "utf8", function (error, data) {
   for (let i = 0; i < sortedSeparator.length; i++) {
     query += "'" + sortedSeparator[i] + "',";
   }
-  fs.appendFile("finalpastas.txt", query, function (error0) {
+  fs.appendFile("rupastas.txt", query, function (error0) {
     if (error0) {
       console.log(error0);
     }
